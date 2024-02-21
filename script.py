f1b6e1d5895ee864077ca175a475b37014df0279
Sam <113455539+SamShrubo@users.noreply.github.com>
2024-02-21 00:34:32 -0600

    updated lists

--- script.py
+++ script.py
@@ -29,7 +29,32 @@ itemList = ["Ketchup",
             "Tater Tots",
             "Cold Brew",
             "Coffee Grounds",
+            "Cheese",
+            "Sesame Buns",
+            "Cookie Dough",
+            "Flour",
+            "Jalapenos",
+            "Pepsi Syrup",
+            "Starry Syrup",
+            "Gatorade Syrup",
+            "Dr Pepper Syrup",
+            "Diet Pepsi Syrup",
+            "Chicken Patty",
+            "Potatoes"
             ]
+
+menuFooditems = ["Hamburger",
+                 "Cheeseburger",
+                 "Chicken Sandwich",
+                 "5 Chicken Tenders Box",
+                 "3 Chicken Tenders Box",
+                 "Salad",
+                 "Soda",
+                 "Icecream",
+                 "Icecream Sundae",
+                 "Cookie",
+                 "Onion Rings",
+                 "Fries"]
 
 with open("fill.sql", "w") as fd:
     # fd.write("CREATE TABLE db("
@@ -40,3 +65,4 @@ with open("fill.sql", "w") as fd:
         fd.write(f"INSERT INTO ingredient (\"ingredientID\", \"name\", stock)\nVALUES ({i+1}, '{itemList[i]}', {random.randint(0, 150)});\n")
 
     fd.write(f"INSERT INTO food (\"foodID\", name, price, \"foodType\")\nVALUES (69, 'Hamburger', 11.99, 'Burger');\n")
+    
