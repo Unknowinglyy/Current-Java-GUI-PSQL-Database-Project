3f9e17454cbefda0e33484c8c237a445d051e4d7
Sam <113455539+SamShrubo@users.noreply.github.com>
2024-02-20 23:46:35 -0600

    updated script.py

--- fill.sql
+++ fill.sql
@@ -1,58 +1,60 @@
 INSERT INTO ingredient ("ingredientID", "name", stock)
-VALUES (1, 'Ketchup', 46);
+VALUES (1, 'Ketchup', 29);
 INSERT INTO ingredient ("ingredientID", "name", stock)
-VALUES (2, 'Mayo', 62);
+VALUES (2, 'Mayo', 109);
 INSERT INTO ingredient ("ingredientID", "name", stock)
-VALUES (3, 'Ranch', 54);
+VALUES (3, 'Ranch', 95);
 INSERT INTO ingredient ("ingredientID", "name", stock)
-VALUES (4, 'Gig em Sauce', 127);
+VALUES (4, 'Gig em Sauce', 116);
 INSERT INTO ingredient ("ingredientID", "name", stock)
-VALUES (5, 'Buffalo Sauce', 51);
+VALUES (5, 'Buffalo Sauce', 143);
 INSERT INTO ingredient ("ingredientID", "name", stock)
-VALUES (6, 'Honey Mustard', 50);
+VALUES (6, 'Honey Mustard', 55);
 INSERT INTO ingredient ("ingredientID", "name", stock)
-VALUES (7, 'Spicy Ranch', 53);
+VALUES (7, 'Spicy Ranch', 88);
 INSERT INTO ingredient ("ingredientID", "name", stock)
-VALUES (8, 'Pickles', 88);
+VALUES (8, 'Pickles', 91);
 INSERT INTO ingredient ("ingredientID", "name", stock)
-VALUES (9, 'Patties', 117);
+VALUES (9, 'Patties', 10);
 INSERT INTO ingredient ("ingredientID", "name", stock)
-VALUES (10, 'Chicken Tenders', 150);
+VALUES (10, 'Chicken Tenders', 106);
 INSERT INTO ingredient ("ingredientID", "name", stock)
-VALUES (11, 'Fries', 75);
+VALUES (11, 'Fries', 146);
 INSERT INTO ingredient ("ingredientID", "name", stock)
-VALUES (12, 'Bun', 54);
+VALUES (12, 'Bun', 138);
 INSERT INTO ingredient ("ingredientID", "name", stock)
-VALUES (13, 'Lettuce', 39);
+VALUES (13, 'Lettuce', 129);
 INSERT INTO ingredient ("ingredientID", "name", stock)
-VALUES (14, 'Tomato', 17);
+VALUES (14, 'Tomato', 57);
 INSERT INTO ingredient ("ingredientID", "name", stock)
-VALUES (15, 'Onion', 21);
+VALUES (15, 'Onion', 121);
 INSERT INTO ingredient ("ingredientID", "name", stock)
-VALUES (16, 'Vanilla Ice Cream', 36);
+VALUES (16, 'Vanilla Ice Cream', 95);
 INSERT INTO ingredient ("ingredientID", "name", stock)
-VALUES (17, 'Chocolate Ice Cream', 102);
+VALUES (17, 'Chocolate Ice Cream', 98);
 INSERT INTO ingredient ("ingredientID", "name", stock)
-VALUES (18, 'Strawberry Ice Cream', 134);
+VALUES (18, 'Strawberry Ice Cream', 94);
 INSERT INTO ingredient ("ingredientID", "name", stock)
-VALUES (19, 'Brownie', 32);
+VALUES (19, 'Brownie', 123);
 INSERT INTO ingredient ("ingredientID", "name", stock)
-VALUES (20, 'Bacon', 122);
+VALUES (20, 'Bacon', 1);
 INSERT INTO ingredient ("ingredientID", "name", stock)
-VALUES (21, 'Pepper Jack Cheese', 75);
+VALUES (21, 'Pepper Jack Cheese', 9);
 INSERT INTO ingredient ("ingredientID", "name", stock)
-VALUES (22, 'Texas Toast', 7);
+VALUES (22, 'Texas Toast', 52);
 INSERT INTO ingredient ("ingredientID", "name", stock)
-VALUES (23, 'Black Bean Patty', 33);
+VALUES (23, 'Black Bean Patty', 94);
 INSERT INTO ingredient ("ingredientID", "name", stock)
-VALUES (24, 'Kettle Chips', 137);
+VALUES (24, 'Kettle Chips', 106);
 INSERT INTO ingredient ("ingredientID", "name", stock)
-VALUES (25, 'Gravy', 77);
+VALUES (25, 'Gravy', 56);
 INSERT INTO ingredient ("ingredientID", "name", stock)
-VALUES (26, 'Onion Rings', 47);
+VALUES (26, 'Onion Rings', 58);
 INSERT INTO ingredient ("ingredientID", "name", stock)
-VALUES (27, 'Tater Tots', 129);
+VALUES (27, 'Tater Tots', 121);
 INSERT INTO ingredient ("ingredientID", "name", stock)
-VALUES (28, 'Cold Brew', 52);
+VALUES (28, 'Cold Brew', 118);
 INSERT INTO ingredient ("ingredientID", "name", stock)
-VALUES (29, 'Coffee Grounds', 130);
+VALUES (29, 'Coffee Grounds', 136);
+INSERT INTO food ("foodID", name, price, "foodType")
+VALUES (69, 'Hamburger', 11.99, 'Burger');
