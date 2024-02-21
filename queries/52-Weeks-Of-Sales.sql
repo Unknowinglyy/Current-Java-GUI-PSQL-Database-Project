5bc9c47112790e12ac578ebcfd58b7a3ad627673
donghacho <113952114+donghacho@users.noreply.github.com>
2024-02-21 12:12:32 -0600

    Update 52-Weeks-Of-Sales.sql

--- queries/52-Weeks-Of-Sales.sql
+++ queries/52-Weeks-Of-Sales.sql
@@ -1,5 +1,5 @@
 SELECT 
-  (DATE_PART('year', "timeOrdered") - 2023) * 52 + DATE_PART('week', "timeOrdered") AS week_number, 
+  (DATE_PART('year', "timeOrdered") - 1979) * 52 + DATE_PART('week', "timeOrdered") AS week_number, 
   COUNT(*) AS order_count
 FROM ticket
 GROUP BY week_number
