fa173199373914a32e02f6036a230c20bbe21db9
shantanold <113400565+shantanold@users.noreply.github.com>
2024-02-21 00:40:22 -0600

    updated syntax
    fixed syntax on sql queries.

--- Revenue-by-food-type.sql
+++ Revenue-by-food-type.sql
@@ -1,5 +1,5 @@
 SELECT f."foodType", SUM(t."totalCost") AS total_revenue
 FROM food f
-JOIN "foodTicket" ft ON f."foodID" = ft."foodID"
-JOIN ticket t ON ft."ticketID" = t.id
+JOIN "foodticket" ft ON f."foodID" = ft."foodID"
+JOIN ticket t ON ft."ticketID" = t."ticketID"
 GROUP BY f."foodType";
