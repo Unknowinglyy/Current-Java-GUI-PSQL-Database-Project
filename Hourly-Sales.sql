9d20a4e440360bf1e96bad6f8511403117f8642c
shantanold <113400565+shantanold@users.noreply.github.com>
2024-02-21 00:31:28 -0600

    modified some queries to adjust to new syntax
    as title says

--- Hourly-Sales.sql
+++ Hourly-Sales.sql
@@ -1,4 +1,6 @@
-SELECT EXTRACT(HOUR FROM t."timeOrdered") AS hour_of_day, COUNT(t.id) AS order_count, SUM(t."totalCost") AS total_amount
-FROM ticket t
-GROUP BY hour_of_day
-ORDER BY hour_of_day;
+SELECT DATE_TRUNC('hour', "timeOrdered") AS hour_timestamp, 
+       COUNT(*) AS order_count,
+       SUM("totalCost") AS total_sales
+FROM ticket
+GROUP BY hour_timestamp
+ORDER BY hour_timestamp;
