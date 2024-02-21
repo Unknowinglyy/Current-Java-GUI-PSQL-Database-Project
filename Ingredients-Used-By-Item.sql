f72706172186af70652f4bf2ff2ad41bbd22e5fd
shantanold <113400565+shantanold@users.noreply.github.com>
2024-02-21 00:41:51 -0600

    fixed some more query syntax.

--- Ingredients-Used-By-Item.sql
+++ Ingredients-Used-By-Item.sql
@@ -2,4 +2,5 @@ SELECT i."name" AS ingredient_name
 FROM foodingredient fi
 JOIN ingredient i ON fi."ingredientID" = i."ingredientID"
 JOIN food f ON fi."foodID" = f."foodID"
-WHERE f."name" = 'specific_food_item';
+WHERE f."name" = 'Hamburger';
+--lists ingredients used by each item
