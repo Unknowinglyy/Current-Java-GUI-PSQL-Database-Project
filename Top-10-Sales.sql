1621201c04b1b7c1414fd206d06e57f2c60457b2
shantanold <113400565+shantanold@users.noreply.github.com>
2024-02-21 00:40:22 -0600

    updated syntax
    fixed syntax on sql queries.

--- Top-10-Sales.sql
+++ Top-10-Sales.sql
@@ -1,4 +1,4 @@
-SELECT DATE(t."timeOrdered") AS order_date, SUM(t."totalCost") AS total_amount
+SELECT DATE_TRUNC('day', t."timeOrdered") AS order_date, SUM(t."totalCost") AS total_amount
 FROM ticket t
 GROUP BY order_date
 ORDER BY total_amount DESC
