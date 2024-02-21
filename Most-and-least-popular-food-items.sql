69a27dc80195b8bf4e3efd9d63e7e83647f43b93
NAP2736 <99851978+NAP2736@users.noreply.github.com>
2024-02-21 00:35:27 -0600

    Top five ingredients iteration 1

--- Most-and-least-popular-food-items.sql
+++ Most-and-least-popular-food-items.sql
@@ -4,3 +4,12 @@ JOIN "foodTicket" ft ON f."foodID" = ft."foodID"
 GROUP BY f.name
 ORDER BY order_count DESC
 LIMIT 5;
+
+UNION ALL
+
+SELECT f.name AS food_item, COUNT(ft."ticketID") AS order_count
+FROM food f
+JOIN "foodTicket" ft ON f."foodID" = ft."foodID"
+GROUP BY f.name
+ORDER BY order_count
+LIMIT 5;
