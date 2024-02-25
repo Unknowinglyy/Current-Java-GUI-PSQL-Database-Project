9272476490cf91227cd6d2cbacececf88563d979
Dongha Cho <donghatamu@tamu.edu>
2024-02-25 13:34:06 -0600

    update

--- mainMenu.java
+++ mainMenu.java
@@ -9,20 +9,20 @@ class mainMenu {
         headerLabel.setBounds(1000, 50, 400, 20); 
         f.add(headerLabel);
 
-        Button b1 = new Button("Burger");
+        Button b1 = new Button("Hamburger");
         b1.setBounds(1000, 90, 200, 200);
         f.add(b1);
 
-        Button b2 = new Button("Cheeseburger");
+        Button b2 = new Button("Rev Burger");
         b2.setBounds(1200, 90, 200, 200);
         f.add(b2);
        
-        Button b3 = new Button("Tender");
+        Button b3 = new Button("Bacon Burger");
         b3.setBounds(1400, 90, 200, 200);
         f.add(b3);
 
-        Button b4 = new Button("Sandwich"); // Corrected variable name
-        b4.setBounds(1600, 90, 200, 200); // Corrected button position
+        Button b4 = new Button("Cheeseburger");
+        b4.setBounds(1600, 90, 200, 200); 
         f.add(b4);
 
         f.setSize(3000, 3000);
