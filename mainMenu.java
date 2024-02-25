274593bbb79d5cd125a9ff074415e1a5deea22cb
donghacho <113952114+donghacho@users.noreply.github.com>
2024-02-25 13:09:10 -0600

    Update mainMenu.java

--- mainMenu.java
+++ mainMenu.java
@@ -1,1 +1,32 @@
-
+import java.awt.*;
+ 
+class mainMenu {
+    mainMenu()
+    {
+        Frame f = new Frame();
+ 
+        Label headerLabel = new Label("Select Food:");
+        headerLabel.setBounds(100, 20, 400, 20); 
+        f.add(headerLabel);
+ 
+        Button b1 = new Button("Burger");
+        b1.setBounds(100, 50, 200, 200);
+        f.add(b1);
+ 
+    
+        Button b2 = new Button("Cheeseburger");
+        b2.setBounds(200, 50, 200, 200);
+        f.add(b2);
+       
+       
+        Button b3 = new Button("Tender");
+        b3.setBounds(300, 50, 200, 200);
+        f.add(b3);
+ 
+        f.setSize(2000, 2000);
+        f.setLayout(null);
+        f.setVisible(true);
+    }
+ 
+    public static void main(String a[]) { new mainMenu(); }
+}
