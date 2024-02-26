e863f7c9ab97fca4ee78d56eca90d5d2c6752dca
Blake Dejohn <unknowingly@tamu.edu>
2024-02-25 19:53:29 -0600

    updated one of the queries

--- queries/52-Weeks-Of-Sales.sql
+++ queries/52-Weeks-Of-Sales.sql
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-SELECT DATE_PART('week', "timeOrdered") AS week_number, COUNT(*) AS order_count
-FROM ticket
-GROUP BY week_number
-ORDER BY week_number;
---lists orders per week
-=======
 SELECT 
   (DATE_PART('year', "timeOrdered") - 2022) * 52 + DATE_PART('week', "timeOrdered") AS week_number, 
   COUNT(*) - 7 AS order_count
@@ -12,4 +5,3 @@ FROM ticket
 GROUP BY week_number
 ORDER BY week_number;
 --lists orders per week
->>>>>>> 274593bbb79d5cd125a9ff074415e1a5deea22cb
