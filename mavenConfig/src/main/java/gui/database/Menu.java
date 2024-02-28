420a3ef3c98cee06c1169cb6da76a754513b6864
NAP2736 <99851978+NAP2736@users.noreply.github.com>
2024-02-28 00:30:24 -0600

    added all the db support for menu.java

--- mavenConfig/src/main/java/gui/database/Menu.java
+++ mavenConfig/src/main/java/gui/database/Menu.java
@@ -1,118 +1,397 @@
 package gui.database;
 
+package gui.database;
+
 import java.util.*;
+import java.sql.*;
 
 public class Menu { 
-    //types of food used to find index for Food Items
-    Vector<String> FoodTypes = new Vector<String>(1);
-    //Specific Foods sorted by Food types Used to find index for Ingredients
-    Vector<Vector<String>> FoodItems = new Vector<Vector<String>>(1);
-    //Ingredients for specific foods sorted by food type then food
-    Vector<Vector<Vector<String>>> Ingredients = new Vector<Vector<Vector<String>>>(1);
+    
+    Connection conn = null;
+    String database_name = "csce331_902_01_db";
+    String database_user = "csce331_902_01_user";
+    String database_password = "EPICCSCEPROJECT";
+    String database_url = String.format("jdbc:postgresql://csce-315-db.engr.tamu.edu/%s", database_name);
 
+    //checked
     public void AddFood(String FoodName, String FoodCatagory, Double Price, Vector<String> Recipe){
         //checks if Food type already exists
-        if(FoodTypes.indexOf(FoodCatagory) == -1){
-            FoodTypes.addElement(FoodCatagory);
-            Vector<String> blank = new Vector<String>();
-            FoodItems.addElement(blank);
-            Vector<Vector<String>> blanker = new Vector<Vector<String>>();
-            Ingredients.addElement(blanker);
-        }
+        //"INSERT INTO food (\"foodID\", name, price, \"foodType\")\nVALUES ({foodID}, 'Hamburger', 11.99, 'Burger');\n"
+        
+        //connect to database
+        
+        //trys to add the food to order
+        try {
+            conn = DriverManager.getConnection(database_url, database_user, database_password);
+            String sql = "SELECT MAX(\"foodID\") AS highest_id FROM food";
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
 
-        //finds the location
-        int typeIndex = FoodTypes.indexOf(FoodCatagory);
-        //adds the food to the list of foods under the catagory
-        FoodItems.get(typeIndex).addElement(FoodName);
-        //adds the price to the ingredients
-        Recipe.add(0,Double.toString(Price));
-        //adds the ingredients for the food
-        Ingredients.get(typeIndex).addElement(Recipe);
+            int highestId = 0; // Initialize with default value
+            if (rs.next()) {
+                highestId = rs.getInt("highest_id");
+            }
+
+            rs.close();
+            stmt.close();
+            int newId = highestId +1;
+            String insertQuery = "INSERT INTO food (\"foodID\", name, price, \"foodType\",onmenu) VALUES (?, ?, ?, ?, ?)";
+            PreparedStatement pstmt = conn.prepareStatement(insertQuery);
+
+            pstmt.setInt(1, newId); //set id
+            pstmt.setString(2, FoodName); // Set the name
+            pstmt.setDouble(3, Price); // Set the price
+            pstmt.setString(4, FoodCatagory); //set catagory
+            pstmt.setInt(5, 1); // Set onMenu
+
+            pstmt.executeUpdate();
+            pstmt.close();
+            conn.close();
+
+            //goes through all ingredients and creates the relationship between food and the item
+            for(int i =0; i < Recipe.size();i++){
+                conn = DriverManager.getConnection(database_url, database_user, database_password);
+                int ingredientId = findOrCreateIngredient(Recipe.get(i));
+                String ingredientQuery = "INSERT INTO foodingredient (\"foodID\", \"ingredientID\", \"amount\") VALUES (?, ?, ?)";
+                PreparedStatement pstmt2 = conn.prepareStatement(ingredientQuery);
+
+                pstmt2.setInt(1, newId); //set the ingredient id
+                pstmt2.setInt(2, ingredientId); // Set the food id
+                pstmt2.setInt(3, 1); // Set the amount
+                
+
+                pstmt2.executeUpdate();
+                pstmt2.close();
+                conn.close();
+            }
+
+            
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
     }
+
+    public int findIngredientId(String ingredientName){
+        int ingredientId = -1;
+        try {
+            conn = DriverManager.getConnection(database_url, database_user, database_password);
+            String sql = "SELECT \"ingredientID\" FROM ingredient WHERE name = ?";
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, ingredientName);
+            ResultSet rs = pstmt.executeQuery();
+            if (rs.next()) {
+                ingredientId = rs.getInt("ingredientID");
+            }
+            rs.close();
+            pstmt.close();
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return ingredientId;
+    }
+
+    public int findFoodId(String foodName) {
+        int foodId = -1;
+        try {
+            conn = DriverManager.getConnection(database_url, database_user, database_password);
+            String sql = "SELECT \"foodID\" FROM food WHERE name = ? and onmenu = 1";
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, foodName);
+            ResultSet rs = pstmt.executeQuery();
+            if (rs.next()) {
+                foodId = rs.getInt("foodID");
+            }
+            rs.close();
+            pstmt.close();
+            conn.close();
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return foodId;
+    }
+
+    public String getIngredientNameFromID(int IngredientId) {
+        String foodName = "";
+        try {
+            conn = DriverManager.getConnection(database_url, database_user, database_password);
+            String sql = "SELECT name FROM ingredient WHERE \"ingredientID\" = ?";
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setInt(1, IngredientId);
+            ResultSet rs = pstmt.executeQuery();
+            if (rs.next()) {
+                foodName = rs.getString("name");
+            }
+            rs.close();
+            pstmt.close();
+            conn.close();
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return foodName;
+    }
+
+    private int findOrCreateIngredient(String ingredientName){
+        // Check if the ingredient already exists
+        int ingredientId = findIngredientId(ingredientName);
+        try {
+            conn = DriverManager.getConnection(database_url, database_user, database_password);
+            
+            if (ingredientId != -1) {
+                return ingredientId;
+            }
+
+            //if not finds the highest id
+            String sql = "SELECT MAX(\"ingredientID\") AS highest_id FROM ingredient";
+            Statement stmt = conn.createStatement();
+            ResultSet rs2 = stmt.executeQuery(sql);
+
+            int highestId = 0; // Initialize with default value
+            if (rs2.next()) {
+                highestId = rs2.getInt("highest_id");
+            }
+
+            rs2.close();
+            stmt.close();
+            //gets 1 higher to avoid conflicts
+            int newId = highestId +1;
+
+            // If the ingredient doesn't exist, create it
+            String sql2 = "INSERT INTO ingredient (\"ingredientID\",name,stock) VALUES (?,?,?)";
+            PreparedStatement pstmt = conn.prepareStatement(sql2, Statement.RETURN_GENERATED_KEYS);
+            pstmt.setInt(1, newId);
+            pstmt.setString(2,ingredientName);
+            pstmt.setInt(3, 0);
+            pstmt.executeUpdate();
+
+            // Retrieve the auto-generated ID of the newly inserted ingredient
+            ResultSet rs = pstmt.getGeneratedKeys();
+            if (rs.next()) {
+                ingredientId = rs.getInt(1);
+            }
+            rs.close();
+            pstmt.close();
+            conn.close();
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        
+        return ingredientId;
+    }
+
 
     //returns the catagorie of a food
-    public String GetFoodCatagory(String FoodName){
-        //goes through each catagory and checks if the food is in it
-        for(int i = 0; i < FoodItems.size();i++){
-            //if it find the item of the name return the catagory
-            if(FoodItems.get(i).indexOf(FoodName) != -1){
-                String FoodType = FoodTypes.get(i);
-                return(FoodType);
+    public String GetFoodCatagory(String foodName){
+        String foodCatagory = "";
+        try {
+            conn = DriverManager.getConnection(database_url, database_user, database_password);
+            String sql = "SELECT \"foodType\" FROM food WHERE name = ? and onmenu = 1";
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, foodName);
+            ResultSet rs = pstmt.executeQuery();
+            if (rs.next()) {
+                foodCatagory = rs.getString("foodType");
             }
+            rs.close();
+            pstmt.close();
+            conn.close();
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
-        return("");
+        return foodCatagory;
     }
 
-    public Vector<String> GetRecipe(String FoodName){
-        //finds the food in ingredients
-        String Catagory = GetFoodCatagory(FoodName);
-        int CatIndex = FoodTypes.indexOf(Catagory);
-        int foodIndex = FoodItems.get(CatIndex).indexOf(FoodName);
-        Vector<String> Recipe = Ingredients.get(CatIndex).get(foodIndex);
-        //gets rid of the price
-        Recipe.remove(0);
+    public Vector<String> GetRecipe(String foodName){
+        int FoodID = findFoodId(foodName);
+        Vector<String> Recipe = new Vector<String>(1);
+        try {
+            conn = DriverManager.getConnection(database_url, database_user, database_password);
+            String sql = "SELECT \"ingredientID\" FROM foodIngredient WHERE \"foodID\" = ?";
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setInt(1, FoodID);
+            ResultSet rs = pstmt.executeQuery();
+            Vector<Integer> tempIngredientsID = new Vector<Integer>(1);
+            while (rs.next()) {
+                int ingredientID = rs.getInt("ingredientID");
+                tempIngredientsID.add(ingredientID);
+            }
+            
+            //converts int ingredients to names
+            
+            for(int i = 0; i < tempIngredientsID.size();i++){
+                String ingredientName = getIngredientNameFromID(tempIngredientsID.get(i));
+                Recipe.add(ingredientName);
+            }
+            rs.close();
+            pstmt.close();
+            conn.close();
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
         return(Recipe);
     }
 
     //all foods of a food type
     public Vector<String> GetFoodFromFoodType(String Catagory){
-        //finds the food in ingredients
-        int CatIndex = FoodTypes.indexOf(Catagory);
-        return(FoodItems.get(CatIndex));
+        // SQL query to retrieve distinct food types where onMenu = 1
+        // Create a statement
+        
+        Vector<String> tempFoodNames = new Vector<String>(1);
+        try {
+            conn = DriverManager.getConnection(database_url, database_user, database_password);
+            conn = DriverManager.getConnection(database_url, database_user, database_password);
+            String sql = "SELECT \"name\" FROM food WHERE \"foodType\" = ? and onmenu = 1";
+            PreparedStatement stmt = conn.prepareStatement(sql , Statement.RETURN_GENERATED_KEYS);
+            stmt.setString(1, Catagory);
+                // Execute the query and get the result set
+            ResultSet rs = stmt.executeQuery();
+            
+
+            
+            // Process the result set and add food types to the Vector
+            while (rs.next()) {
+                String foodName = rs.getString("name");
+                tempFoodNames.add(foodName);
+            }
+
+            // Close the result set, statement, and connection
+            rs.close();
+            stmt.close();
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return(tempFoodNames);
     }
     
     //gives all food types
     public Vector<String> GetFoodTypes(){
-        return(FoodTypes);
+        // SQL query to retrieve distinct food types where onMenu = 1
+        String sql = "SELECT DISTINCT \"foodType\" FROM food WHERE onMenu = 1";
+
+        // Create a statement
+        Statement stmt;
+        Vector<String> tempFoodTypes = new Vector<String>(1);
+        try {
+            conn = DriverManager.getConnection(database_url, database_user, database_password);
+            stmt = conn.createStatement();
+                // Execute the query and get the result set
+            ResultSet rs = stmt.executeQuery(sql);
+
+            
+            // Process the result set and add food types to the Vector
+            while (rs.next()) {
+                String foodType = rs.getString("foodType");
+                tempFoodTypes.add(foodType);
+            }
+
+            // Close the result set, statement, and connection
+            rs.close();
+            stmt.close();
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return(tempFoodTypes);
     }
-    public Double GetPrice(String FoodName){
-        //finds the food
-        String Catagory = GetFoodCatagory(FoodName);
-        int CatIndex = FoodTypes.indexOf(Catagory);
-        int foodIndex = FoodItems.get(CatIndex).indexOf(FoodName);
-        Vector<String> Recipe = Ingredients.get(CatIndex).get(foodIndex);
-        //gets the price
-        Double Price = Double.parseDouble(Recipe.get(0));
-        return(Price);
+    public Double GetPrice(String foodName){
+        Double foodPrice = 0.0;
+        try {
+            conn = DriverManager.getConnection(database_url, database_user, database_password);
+            String sql = "SELECT \"price\" FROM food WHERE name = ? and onmenu = 1";
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, foodName);
+            ResultSet rs = pstmt.executeQuery();
+            if (rs.next()) {
+                foodPrice = rs.getDouble("price");
+            }
+            rs.close();
+            pstmt.close();
+            conn.close();
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return foodPrice;
     }
 
     //changes the price of a food
     public void ChangePrice(String FoodName, Double Price){
-        //finds the food
-        String Catagory = GetFoodCatagory(FoodName);
-        int CatIndex = FoodTypes.indexOf(Catagory);
-        int foodIndex = FoodItems.get(CatIndex).indexOf(FoodName);
-        //changes the food price to the new Price
-        Ingredients.get(CatIndex).get(foodIndex).set(0,Double.toString(Price));
+        try (Connection conn = DriverManager.getConnection(database_url, database_user, database_password)) {
+            // SQL query to update the price of the food item
+            String sql = "UPDATE food SET price = ? WHERE \"foodID\" = ?";
+            
+            // Create a PreparedStatement
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            int foodID = findFoodId(FoodName);
+            // Set the parameters
+            pstmt.setDouble(1, Price);
+            pstmt.setInt(2, foodID);
+            
+            // Execute the update
+            int rowsUpdated = pstmt.executeUpdate();
+            
+            if (rowsUpdated > 0) {
+                System.out.println("Price for food item with foodID " + foodID + " updated successfully.");
+            } else {
+                System.out.println("No food item found with foodID " + foodID + ".");
+            }
+
+            // Close the statement and connection
+            pstmt.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
+
 
     //removes a food
     public void RemoveFood(String FoodName){
-        //finds teh food
-        String Catagory = GetFoodCatagory(FoodName);
-        int CatIndex = FoodTypes.indexOf(Catagory);
-        int foodIndex = FoodItems.get(CatIndex).indexOf(FoodName);
-        //removes the food
-        Ingredients.get(CatIndex).remove(foodIndex);
-        FoodItems.remove(foodIndex);
-    }
+        try (Connection conn = DriverManager.getConnection(database_url, database_user, database_password)) {
+            // SQL query to update the price of the food item
+            String sql = "UPDATE food SET \"onmenu\" = 0 WHERE \"foodID\" = ? and onmenu = 1";
+            
+            // Create a PreparedStatement
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            int foodID = findFoodId(FoodName);
+            // Set the parameters
+            pstmt.setInt(1, foodID);
+            
+            // Execute the update
+            int rowsUpdated = pstmt.executeUpdate();
+            
+            if (rowsUpdated > 0) {
+                System.out.println("Price for food item with foodID " + foodID + " updated successfully.");
+            } else {
+                System.out.println("No food item found with foodID " + foodID + ".");
+            }
 
-    //removes a food type
-    public void RemoveFoodType(String Catagory){
-        int CatIndex = FoodTypes.indexOf(Catagory);
-        FoodTypes.remove(CatIndex);
-        FoodItems.remove(CatIndex);
-        Ingredients.remove(CatIndex);
+            // Close the statement and connection
+            pstmt.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        
     }
 
     //adds a the basic rev menu
-    public void GenerateBasicMenu(){
-        addBurgers();
-        addBaskets();
-        addSandwiches();
-        addSides();
-        addSauces();
-        addBeverages();
+    public void GenerateBasicMenu() {
+        //RemoveFood("Double stack cheeseburger");
+        // addBurgers();
+        // addBaskets();
+        // addSandwiches();
+        // addSides();
+        // addSauces();
+        // addBeverages();
     }
 
     // Helper methods to add different categories of items to the menu
